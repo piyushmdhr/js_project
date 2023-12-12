f63eaf9c0d1640058b10f4a5ac2019c8da6a04b6
piyushmdhr <you@example.com>
2023-12-11 20:30:20 -0500

    added js functions

--- js/product-details.js
+++ js/product-details.js
@@ -1,6 +1,32 @@
 $(document).ready(() => {
+  $("#lessItem").click(() => {
+    let itemQuantity = parseInt($("#itemQuantity").val());
+    if (itemQuantity >= 2) {
+      console.log(" HEre");
+      itemQuantity = itemQuantity - 1;
+
+      console.log(itemQuantity);
+
+      $("#itemQuantity").val(itemQuantity);
+    }
+  });
+  $("#moreItem").click(() => {
+    let itemQuantity = parseInt($("#itemQuantity").val());
+
+    if (itemQuantity < 5) {
+      console.log("alsi HEre");
+      itemQuantity = itemQuantity + 1;
+
+      console.log(itemQuantity);
+
+      $("#itemQuantity").val(itemQuantity);
+    }
+  });
+
+  // jquery ui tabs
   $("#tabs").tabs();
 
+  // image slider
   $("#content-slider").lightSlider({
     loop: true,
     keyPress: true,
@@ -16,5 +42,22 @@ $(document).ready(() => {
     onSliderLoad: function () {
       $("#image-gallery").removeClass("cS-hidden");
     },
+  });
+
+  //dialog
+  $("#dialog").dialog({
+    autoOpen: false,
+    show: {
+      effect: "blind",
+      duration: 1000,
+    },
+    hide: {
+      effect: "explode",
+      duration: 1000,
+    },
+  });
+
+  $("#cart").on("click", function () {
+    $("#dialog").dialog("open");
   });
 });
